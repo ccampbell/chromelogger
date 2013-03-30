75acfb11dfbb38fe4ef33f90dae642c8026278c0
Craig Campbell <iamcraigcampbell@gmail.com>
2013-03-30 15:33:35 -0400

    More cleanup
    - Break out Base64 into its own file
    - Fix jshint warnings

--- log.js
+++ log.js
@@ -1,10 +1,12 @@
+// global chrome, console, Base64
 /**
- * singleton class for handling all the logging magic
+ * code for handling all the logging magic
  *
  * @author Craig Campbell <iamcraigcampbell@gmail.com>
  */
-var ChromePhpLogger = function()
-{
+(function() {
+    'use strict';
+
     /**
      * @var string
      */
@@ -14,11 +16,6 @@ var ChromePhpLogger = function()
      * @var object
      */
     var local_storage = null;
-
-    /**
-     * @var array
-     */
-    var request_times = [];
 
     /**
      * @var array
@@ -61,7 +58,9 @@ var ChromePhpLogger = function()
      */
     function _logCleanData(data, callback)
     {
-        column_map = {};
+        var column_map = {};
+        var column_name;
+
         for (var key in data.columns) {
             column_name = data.columns[key];
             column_map[column_name] = key;
@@ -159,8 +158,7 @@ var ChromePhpLogger = function()
      */
     function _jsonDecode(json_string)
     {
-        data = JSON.parse(json_string);
-        return data;
+        return JSON.parse(json_string);
     }
 
     function _decode(header) {
@@ -172,7 +170,7 @@ var ChromePhpLogger = function()
             match = false,
             header = '';
 
-        for (var i = 0; i < headers.length; ++i) {
+        for (var i = 0; i < headers.length; i++) {
             if (headers[i].name.toLowerCase() == HEADER_NAME) {
                 header = headers[i].value;
                 match = true;
@@ -184,12 +182,12 @@ var ChromePhpLogger = function()
             return;
         }
 
-        data = _decode(header);
+        var data = _decode(header);
         _logData(data);
     }
 
     function _handleHeaderUpdate(request, sender, sendResponse) {
-        // if this is not a cookie update don't do anything
+        // if this is not a header update don't do anything
         if (request.name != "header_update") {
             return;
         }
@@ -211,268 +209,28 @@ var ChromePhpLogger = function()
         chrome.extension.onRequest.removeListener(_handleHeaderUpdate);
     }
 
-    return {
-        /**
-         * public run method
-         *
-         * @return void
-         */
-        run : function()
-        {
-            _processQueue(function() {
-                use_queue = false;
-            });
-        },
-
-        /**
-         * set up local storage
-         *
-         * @return void
-         */
-        initStorage : function()
-        {
-            chrome.extension.sendRequest("localStorage", function(response) {
-                local_storage = response;
-                ChromePhpLogger.run();
-            });
-        },
-
-        /**
-         * public init method
-         *
-         * @return void
-         */
-        init : function()
-        {
-            _listenForLogMessages();
-            chrome.extension.sendRequest("isActive", function(response) {
-                if (response === false) {
-                    return _stopListening();
-                }
-                return ChromePhpLogger.initStorage();
-            });
-        }
-    };
-} ();
-
-ChromePhpLogger.init();
-
-/**
- * utility functions used by the logger stuff
- */
-var Util = {
-    /**
-     * trims spaces from either end of a string
-     *
-     * @param string
-     * @return string
-     */
-    trim : function(text)
-    {
-        return (text || "").replace(/^(\s|\u00A0)+|(\s|\u00A0)+$/g, "");
-    },
-
-    /**
-     * gets string position for a piece of a string
-     *
-     * @param string haystack
-     * @param string needle
-     * @param int offset - start searching only after this point in the string
-     * @return int or false
-     */
-    strpos : function(haystack, needle, offset)
-    {
-        var i = (haystack+'').indexOf(needle, (offset || 0));
-        return i === -1 ? false : i;
-    },
-
-    /**
-     * gets a cookie by name
-     *
-     * @param string name of cookie
-     * @return value
-     */
-    getCookie : function(name)
-    {
-        var cookie_value = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = this.trim(cookies[i]);
-
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookie_value = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookie_value;
-    },
-
-    /**
-     * deletes a cookie by name
-     *
-     * @param string name of cookie
-     * @return void
-     */
-    eatCookie : function(name)
-    {
-        document.cookie = name + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
-    },
-
-    /**
-     * checks if a value is in an array
-     *
-     * @param mixed needle
-     * @param array haystack
-     */
-     inArray : function(needle, haystack)
-     {
-        for (var key in haystack) {
-            if (haystack[key] == needle) {
-                return true;
-            }
-        }
-        return false;
-     }
-};
-
-/**
- *
- *  Base64 encode / decode
- *  http://www.webtoolkit.info/
- *
- */
-var Base64 = {
-
-    // private property
-    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-
-    // public method for encoding
-    encode : function (input) {
-        var output = "";
-        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-        var i = 0;
-
-        input = Base64._utf8_encode(input);
-
-        while (i < input.length) {
-
-            chr1 = input.charCodeAt(i++);
-            chr2 = input.charCodeAt(i++);
-            chr3 = input.charCodeAt(i++);
-
-            enc1 = chr1 >> 2;
-            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-            enc4 = chr3 & 63;
-
-            if (isNaN(chr2)) {
-                enc3 = enc4 = 64;
-            } else if (isNaN(chr3)) {
-                enc4 = 64;
-            }
-
-            output = output +
-            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-
-        }
-
-        return output;
-    },
-
-    // public method for decoding
-    decode : function (input) {
-        var output = "";
-        var chr1, chr2, chr3;
-        var enc1, enc2, enc3, enc4;
-        var i = 0;
-
-        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-        while (i < input.length) {
-
-            enc1 = this._keyStr.indexOf(input.charAt(i++));
-            enc2 = this._keyStr.indexOf(input.charAt(i++));
-            enc3 = this._keyStr.indexOf(input.charAt(i++));
-            enc4 = this._keyStr.indexOf(input.charAt(i++));
-
-            chr1 = (enc1 << 2) | (enc2 >> 4);
-            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-            chr3 = ((enc3 & 3) << 6) | enc4;
-
-            output = output + String.fromCharCode(chr1);
-
-            if (enc3 != 64) {
-                output = output + String.fromCharCode(chr2);
-            }
-            if (enc4 != 64) {
-                output = output + String.fromCharCode(chr3);
-            }
-
-        }
-
-        output = Base64._utf8_decode(output);
-
-        return output;
-
-    },
-
-    // private method for UTF-8 encoding
-    _utf8_encode : function (string) {
-        string = string.replace(/\r\n/g,"\n");
-        var utftext = "";
-
-        for (var n = 0; n < string.length; n++) {
-
-            var c = string.charCodeAt(n);
-
-            if (c < 128) {
-                utftext += String.fromCharCode(c);
-            }
-            else if((c > 127) && (c < 2048)) {
-                utftext += String.fromCharCode((c >> 6) | 192);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-            else {
-                utftext += String.fromCharCode((c >> 12) | 224);
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-
-        }
-
-        return utftext;
-    },
-
-    // private method for UTF-8 decoding
-    _utf8_decode : function (utftext) {
-        var string = "";
-        var i = 0;
-        var c = c1 = c2 = 0;
-
-        while ( i < utftext.length ) {
-
-            c = utftext.charCodeAt(i);
-
-            if (c < 128) {
-                string += String.fromCharCode(c);
-                i++;
-            }
-            else if((c > 191) && (c < 224)) {
-                c2 = utftext.charCodeAt(i+1);
-                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                i += 2;
-            }
-            else {
-                c2 = utftext.charCodeAt(i+1);
-                c3 = utftext.charCodeAt(i+2);
-                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                i += 3;
-            }
-        }
-        return string;
+    function _run() {
+        _processQueue(function() {
+            use_queue = false;
+        });
     }
-};
+
+    function _initStorage() {
+        chrome.extension.sendRequest("localStorage", function(response) {
+            local_storage = response;
+            _run();
+        });
+    }
+
+    function _init() {
+        _listenForLogMessages();
+        chrome.extension.sendRequest("isActive", function(response) {
+            if (response === false) {
+                return _stopListening();
+            }
+            return _initStorage();
+        });
+    }
+
+    _init();
+}) ();
