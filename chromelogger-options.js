ea4a2276c23b4ddd00500fe5c68fa168d95275ca
Craig Campbell <iamcraigcampbell@gmail.com>
2013-04-04 01:15:40 -0400

    Refactor options
    - Simplify code
    - Add support for setting custom logging colors

--- chromelogger-options.js
+++ chromelogger-options.js
@@ -2,44 +2,98 @@
 (function() {
     'use strict';
 
-    function saveOptions() {
-        var line_numbers = document.getElementById("show_line_numbers");
-        localStorage.show_line_numbers = line_numbers.checked;
+    var defaults = {
+        show_upgrade_messages: true,
+        show_line_numbers: false,
+        color1: "#888",
+        color2: "#0563ad"
+    };
 
-        var upgrade_messages = document.getElementById("show_upgrade_messages");
-        localStorage.show_upgrade_messages = upgrade_messages.checked;
+    function getInputs() {
+        var checkboxes = document.querySelectorAll('input');
+        return Array.prototype.slice.call(checkboxes);
+    }
 
+    function showMessage(message) {
         var info_div = document.getElementById("info");
-        info_div.innerHTML = "your settings have been saved";
+        info_div.innerHTML = message;
         info_div.style.display = "block";
         setTimeout(function() {
             info_div.style.display = "none";
-        }, 2000);
+        }, 2500);
+
     }
 
-    function restoreDefaults() {
-        var line_numbers = document.getElementById("show_line_numbers");
-        localStorage.show_line_numbers = false;
-        line_numbers.checked = localStorage.show_line_numbers === "true" ? true : false;
+    function saveOptions(e) {
+        e.preventDefault();
 
-        var upgrade_messages = document.getElementById("show_upgrade_messages");
-        localStorage.show_upgrade_messages = true;
-        upgrade_messages.checked = localStorage.show_upgrade_messages === "true" ? true : false;
+        getInputs().forEach(function(input) {
+            if (input.type == 'text') {
+                localStorage[input.name] = _getColorFromValue(input.value);
+                return;
+            }
+            localStorage[input.name] = input.checked;
+        });
+
+        showMessage('your settings have been saved');
+    }
+
+    function _getColorFromValue(value) {
+        if (value.indexOf('#') === 0) {
+            return value;
+        }
+
+        var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'white', 'black'];
+        if (colors.indexOf(value) !== -1) {
+            return value;
+        }
+
+        return '#' + value;
+    }
+
+    function _setInputValue(input, value) {
+        input.value = value.indexOf('#') === 0 ? value.substring(1) : value;
+
+        input.parentNode.querySelector('.swatch').style.background = _getColorFromValue(value);
+    }
+
+    function restoreDefaults(e) {
+        e.preventDefault();
+
+        getInputs().forEach(function(input) {
+            var value = defaults[input.name];
+
+            localStorage[input.name] = value;
+
+            if (input.type == 'text') {
+                _setInputValue(input, value);
+                return;
+            }
+
+            input.checked = value;
+        });
+
+        showMessage('settings have been restored to the defaults');
+    }
+
+    function _handleColorChange(e) {
+        if (e.target.tagName === 'INPUT' && e.target.type == 'text') {
+            _setInputValue(e.target, e.target.value);
+        }
     }
 
     function init() {
-        var line_numbers = document.getElementById("show_line_numbers");
-        line_numbers.checked = localStorage.show_line_numbers === "true" ? true : false;
-
-        var upgrade_messages = document.getElementById("show_upgrade_messages");
-        upgrade_messages.checked = localStorage.show_upgrade_messages === "true" ? true : false;
-
-        if (localStorage.show_upgrade_messages === undefined) {
-            upgrade_messages.checked = true;
-        }
+        getInputs().forEach(function(input) {
+            if (input.type == 'text') {
+                _setInputValue(input, input.name in localStorage ? localStorage[input.name] : defaults[input.name]);
+                return;
+            }
+            input.checked = input.name in localStorage ? localStorage[input.name] === "true" : defaults[input.name];
+        });
 
         document.getElementById('save').addEventListener('click', saveOptions, false);
         document.getElementById('restore').addEventListener('click', restoreDefaults, false);
+        document.addEventListener('input', _handleColorChange, false);
     }
 
     document.addEventListener('DOMContentLoaded', init, false);
